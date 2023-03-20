a00cd7179c476f9e01549e9b928b5c07603f0684
rajahwu <vmradford@gmail.com>
2023-03-20 19:55:47 -0400

    inital commit

---
+++ src/routes/Home.jsx
@@ -0,0 +1,10 @@
+import harvardArt from '../data/harvardArt'
+
+export default function Home() {
+  console.log(harvardArt)
+    return (
+      <div className="App">
+        <h1>Hello From App</h1>
+      </div>
+    );
+  }
